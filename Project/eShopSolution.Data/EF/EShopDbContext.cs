5788425dd34584e0ef7dcaf83c78e1fa0633e0ab
It-Truongthanhtu <truongthanhtu.it.1998@gmail.com>
2021-01-10 21:22:10 +0700

    Create Class Entities

---
+++ Project/eShopSolution.Data/EF/EShopDbContext.cs
@@ -0,0 +1,31 @@
+﻿using eShopSolution.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Data.EF
+{
+    class EShopDbContext:DbContext
+    {
+        public EShopDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        DbSet<Product> Products { get; set; }
+        DbSet<AppConfig> AppConfigs { get; set; }
+        DbSet<Cart> Carts  { get; set; }
+        DbSet<Category> Categorys { get; set; }
+        DbSet<CategoryTranslation> CategoryTranslations { get; set; }
+        DbSet<Contact> Contacts { get; set; }
+        DbSet<Language> Languages  { get; set; }
+        DbSet<Order> Orders { get; set; }
+        DbSet<OrderDetail> OrderDetails { get; set; }
+        DbSet<ProductCategory> ProductCategorys { get; set; }
+        DbSet<ProductTranslation> ProductTranslations{ get; set; }
+        DbSet<Promotion> Promotions { get; set; }
+        DbSet<Transaction> Transactions { get; set; }
+    }   
+
+}
---
+++ Project/eShopSolution.Data/Entities/AppConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Data.Entities
+{
+    class AppConfig
+    {
+        public string Key { get; set; }
+
+        public string Value { get; set; }
+    }
+
+}
