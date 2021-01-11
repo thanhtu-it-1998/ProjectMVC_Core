684faecf8dff59cd8ba02730bbcb33843cb7365c
It-Truongthanhtu <truongthanhtu.it.1998@gmail.com>
2021-01-11 22:58:44 +0700

    Access db with EF

---
+++ Project/eShopSolution.Data/Configurations/AppConfigConfiguration - Copy.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Data.Configuration
+{
+    class AppConfigConfiguration
+    {
+    }
+}
---
+++ Project/eShopSolution.Data/Configurations/CartConfiguration .cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using eShopSolution.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace eShopSolution.Data.Configuration
+{
+    class AppConfigConfiguration:IConventionEntityTypeBuilder<Cart>
+    {
+    }
+}
