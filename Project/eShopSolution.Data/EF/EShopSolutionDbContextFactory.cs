f86578f3342b18b069f6bd0469fe275f368ad2e4
It-Truongthanhtu <truongthanhtu.it.1998@gmail.com>
2021-01-13 00:15:22 +0700

    Update

---
+++ Project/eShopSolution.Data/EF/EShopSolutionDbContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace eShopSolution.Data.EF
+{
+     class EShopDbContextFactory : IDesignTimeDbContextFactory<EShopDbContext>
+    {
+        public EShopDbContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appSet.json")
+                .Build();
+            var connectionString = configuration.GetConnectionString("eShopSolutionDb");
+            var optionsBuilder = new DbContextOptionsBuilder<EShopDbContext>();
+            optionsBuilder.UseSqlServer("Data Source=blog.db");
+
+            return new EShopDbContext(optionsBuilder.Options);
+        }
+    }
+}
